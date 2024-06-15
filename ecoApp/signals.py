d98241c092891a50da08c34faff9c696b3f640b2
Guillemtejonestalella <101114041+Guillemtejonestalella@users.noreply.github.com>
2024-06-15 21:43:45 +0200

    [in progress] logo de la web en el mail automatic

--- ecoApp/signals.py
+++ ecoApp/signals.py
@@ -4,6 +4,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 from order.models import Order
 
+
 @receiver(post_save, sender=Order)
 def update_user_points_and_send_email(sender, instance, **kwargs):  #Actualitza els punts de l'usuari i envia correu de confirmacio.
     if instance.OrderStatus == 'Accepted':        
@@ -50,16 +51,19 @@ def update_user_points_and_send_email(sender, instance, **kwargs):  #Actualitza 
         order.profile.save()
          #Correu electronic automatic Enviament 
 
-        message = (
-            f'Dear {order.username},\n\n'
-            f'Your request has been submitted successfully. Wait to receive the acceptance or denial email.\n\n'
-            f'Thank you for your request!\n\n'
-            f'<img src="{settings.MEDIA_URL}ecoApp/static/asstets/img/logoRedimensionat-transparent.png" alt="Logo">'
+        message= (f'Dear {order.username},\n\n')
+
+        html_message = (
+            f'<p>Dear {order.username},\n\n</p>'
+            f'<p>Your request has been submitted successfully. Wait to receive the acceptance or denial email.\n\n</p>'
+            f'<p>Thank you for your request!\n\n</p>'
+            f'<img src="{settings.MEDIA_URL}ecoApp/static/assets/img/logoRedimensionat-transparent.png" alt="Logo">'
         )
-        message = message
+        
         send_mail(
             subject='Your request has been sended',
             message=message,
+            html_message=html_message,
             from_email=settings.EMAIL_HOST_USER,
             recipient_list=[order.email],
             fail_silently=False,
