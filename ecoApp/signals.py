20a009d0df05cd11a71b1f21727e4975e5010757
Guillemtejonestalella <101114041+Guillemtejonestalella@users.noreply.github.com>
2024-06-15 18:34:56 +0200

    [In progress] username incorporado en la respuesta del mail

--- ecoApp/signals.py
+++ ecoApp/signals.py
@@ -3,41 +3,47 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
 from order.models import Order
+from order.models import User
 
 @receiver(post_save, sender=Order)
 def update_user_points_and_send_email(sender, instance, **kwargs):  #Actualitza els punts de l'usuari i envia correu de confirmacio.
     if instance.OrderStatus == 'Accepted':
-        user = instance.OrderUser        
-        user.profile.ProfilePoints += instance.OrderPoints
-        user.profile.save()
+        
+        order = instance.OrderUser        
+        order.profile.ProfilePoints += instance.OrderPoints
+        order.profile.save()   
+            
 
         #Correu electronic automatic Acceptacio
         send_mail(
             subject='Your order has been ACCEPTED',            
-            message=f'Dear user,\n\nYour request has been accepted! You have been awarded {instance.OrderPoints} points.\n\nThank you for your order!',
+            message=f'Dear {order.username},\n\nYour request has been accepted! You have been awarded {instance.OrderPoints} points.\n\nThank you for your order!',
             from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[user.email],
+            recipient_list=[order.email],
             fail_silently=False,
         )
     elif instance.OrderStatus == 'Denied':
-        user = instance.OrderUser
-        user.profile.save()
+        order = instance.OrderUser
+        order.profile.save()
          #Correu electronic automatic Denegacio
         send_mail(
             subject='Your order has been DENIED',            
-            message=f'Dear user,\n\nYour request has been denied!\n\nResend your request!',
+            message=f'Dear {order.username},\n\nYour request has been denied!\n\nResend your request!',
             from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[user.email],
+            recipient_list=[order.email],
             fail_silently=False,
         )
-    elif instance.OrderStatus == 'Pending approval':
-        user = instance.OrderUser
-        user.profile.save()
+    elif instance.OrderStatus == 'Pending approval':      
+        
+        order = instance.OrderUser
+        order.profile.save()
          #Correu electronic automatic Enviament 
         send_mail(
             subject='Your order has been sended',           
-            message=f'Dear user,\n\nYour request has been submitted successfully. Wait to receive the acceptance or denial email.\n\nThank you for your order!',
+            message=f'Dear {order.username},\n\nYour request has been submitted successfully. Wait to receive the acceptance or denial email.\n\nThank you for your order!',
             from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[user.email],
+            recipient_list=[order.email],
             fail_silently=False,
         )
+# em dona el id {order.user.username}
+        
