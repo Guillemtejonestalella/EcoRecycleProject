1dc0a9130952d5eac1924035aaa8d57a367a52a9
Guillemtejonestalella <101114041+Guillemtejonestalella@users.noreply.github.com>
2024-06-22 11:57:10 +0200

    [Updated] model migrations

--- order/models.py
+++ order/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Order(models.Model): 
-
+    # Menu desplegable per tal que l'admin pugui modificar l'estat de la order
     ORDER_STATUS = (
         ("Pending aproval", "Pending aproval"),
         ("Accepted", "Accepted"),
@@ -14,17 +14,16 @@ class Order(models.Model):
 
     OrderUser = models.ForeignKey(User, on_delete=models.CASCADE)   
     OrderPoints = models.IntegerField()
-    OrderStatus = models.CharField(max_length=70, choices=ORDER_STATUS, default="Pending approval")
-    
+    OrderStatus = models.CharField(max_length=70, choices=ORDER_STATUS, default="Pending approval")    
 
     OrderHomePickup = models.BooleanField(null=True, blank=True)
-    OrderDelivery = models.BooleanField(null=True, blank=True) #new
+    OrderDelivery = models.BooleanField(null=True, blank=True) 
 
     OrderCreationDate = models.DateField() 
     OrderPickupDate = models.DateField(null=True, blank=True)
     
     OrderAddress = models.CharField(max_length=150, null=True, blank=True)
-    OrderDeliveryAddress =  models.CharField(max_length=150, null=True, blank=True) #new
+    OrderDeliveryAddress =  models.CharField(max_length=150, null=True, blank=True) 
     
     def __str__(self): 
         return self.OrderUser.username
--- profiles/models.py
+++ profiles/models.py
@@ -6,18 +6,19 @@ from django.conf import settings
 
 # Create your models here.
 class Profile(models.Model):
-    ProfileName = models.CharField(max_length=50, blank=True)  # Hacemos que sea opcional
-    ProfilePerson = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile') # Cambiamos el nombre del campo
+    ProfileName = models.CharField(max_length=50, blank=True) 
+    ProfilePerson = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile') 
     ProfilePoints = models.IntegerField(default=0)
-    ProfileAdress = models.CharField(max_length=70, blank=True)  # Hacemos que sea opcional
+    ProfileAdress = models.CharField(max_length=70, blank=True) 
 
     def __str__(self): 
         return self.ProfileName
 
+    # Creacio del perfil d'usuari un cop es crea un usuari
     @receiver(post_save, sender=settings.AUTH_USER_MODEL)
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
-            Profile.objects.create(ProfilePerson=instance, ProfileName="profile")  # Establecemos el ProfileName como el username por defecto
+            Profile.objects.create(ProfilePerson=instance, ProfileName="profile")  
 
     @receiver(post_save, sender=settings.AUTH_USER_MODEL)
     def save_user_profile(sender, instance, **kwargs):
